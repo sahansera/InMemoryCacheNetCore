4b7178f5b3a2b820d47e891f6350d5cf7aa8368c
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-08-22 17:01:57 +0930

    Migrate to .NET 6 🎉

--- InMemoryCachingSample/Program.cs
+++ InMemoryCachingSample/Program.cs
@@ -1,23 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using InMemoryCachingSample.Infrastructure;
+using InMemoryCachingSample.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace InMemoryCachingSample
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<UsersService>();
+builder.Services.AddScoped<IUsersService, CachedUserService>();
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder.Services.AddScoped<ICacheProvider, CacheProvider>();
+builder.Services.AddScoped<IHttpClient, HttpClient>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
--- InMemoryCachingSample/Services/CacheService.cs
+++ InMemoryCachingSample/Services/CacheService.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using InMemoryCachingSample.Infrastructure;
 using InMemoryCachingSample.Models;
 using InMemoryCachingSample.Utils;
